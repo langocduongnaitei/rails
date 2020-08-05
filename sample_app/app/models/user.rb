5b5ab7244d563d6374184b6dae824982ffe6c6f6
langocduongnaitei <la.ngoc.duong@sun-asterisk.com>
2020-08-05 09:52:33 +0700

    chapter 11

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
 
   attr_accessor :remember_token, :activation_token
 
-  before_save :downcase_email
   before_create :create_activation_digest
+  before_save :downcase_email
 
   validates :name, presence: true,
     length: {maximum: Settings.user.name.max_length}
@@ -38,11 +38,20 @@ class User < ApplicationRecord
       update :remember_digest, User.digest(remember_token)
     end
 
-    def authenticated? remember_token
+    def authenticated? attribute, token
+      digest = send"#{attribute}_digest"
       return false unless remember_digest
 
       BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
+  end
+
+  def activate
+    update activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   def forget
